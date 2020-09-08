748b9704773075145f7e7b24a1f146af314e6067
taiming <taiming@bridgewell.com>
2020-09-08 22:46:50 +0800

    init

---
+++ src/containers/MainPage/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import data from "./data";
+
+const MainPage = () => {
+  console.log("main page: ", data);
+  return <div>MainPage</div>;
+};
+
+export default MainPage;
