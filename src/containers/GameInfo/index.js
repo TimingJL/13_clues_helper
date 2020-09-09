6fc026d245875caa2dcf5ca752cf64b20454ccd3
taiming <taiming@bridgewell.com>
2020-09-09 14:01:55 +0800

    [feature] modify some style

--- src/containers/GameInfo/index.js
+++ src/containers/GameInfo/index.js
@@ -23,13 +23,32 @@ const images = [
 
 const Container = styled.div`
   height: 100vh;
-  background: black;
   display: flex;
   align-items: center;
+  justify-content: center;
+`;
+
+const Info = styled.div`
+  margin-top: 5vh;
+  color: #c99950;
+  position: absolute;
+  top: 0px;
+  left: 50%;
+  transform: translateX(-50%);
+  font-family: "Noto Sans TC", sans-serif;
+  text-align: center;
+  font-size: 24px;
+  width: 100%;
+  line-height: 36px;
 `;
 
 const GameInfo = () => (
   <Container>
+    <Info>
+      請將手機旋轉至
+      <br />
+      「橫向模式」開始遊戲
+    </Info>
     <ImageGallery
       items={images}
       showThumbnails={false}
