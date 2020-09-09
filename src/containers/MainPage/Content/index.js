373ef77fb07feec374ce23421f7d62ebb6109df7
taiming <taiming@bridgewell.com>
2020-09-09 21:10:20 +0800

    [feature] add icons

--- src/containers/MainPage/Content/index.js
+++ src/containers/MainPage/Content/index.js
@@ -1,8 +1,17 @@
 import React from "react";
 import styled from "styled-components";
+import iconMalePath from "assets/icons/male.png";
+import iconFemalePath from "assets/icons/female.png";
+import InDoorIcon from "assets/icons/InDoorIcon";
+import OutDoorIcon from "assets/icons/OutDoorIcon";
+import UpCloseIcon from "assets/icons/UpCloseIcon";
+import RangedIcon from "assets/icons/RangedIcon";
 import data from "../data";
 
-const MainPageContainer = styled.div``;
+const MainPageContainer = styled.div`
+  font-size: 12px;
+  width: calc(100% - 10px);
+`;
 
 const GridContainer = styled.div`
   display: grid;
@@ -16,11 +25,50 @@ const GridContainer = styled.div`
 const GridBlock = styled.div`
   color: ${(props) => props.textColor};
   background-color: ${(props) => props.backgroundColor};
-  height: ${(props) => props.height}px;
+  ${(props) => {
+    if (props.height) {
+      return `height: ${props.height}px;`;
+    }
+    return null;
+  }}
+  overflow: hidden;
   text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
   &:active {
     opacity: 0.8;
   }
+`;
+
+const Icon = styled.div`
+  background: url(${(props) => props.icon});
+  background-size: cover;
+  background-repeat: no-repeat;
+  width: 20px;
+  height: 20px;
+`;
+
+const IconWrapper = styled.div`
+  background: white;
+  width: 30px;
+  height: 30px;
+  border-radius: 100%;
+  border: 2px solid black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 6px;
+`;
+
+const Text = styled.div`
+  text-shadow: 0px 0px 4px #fff;
+  color: black;
+`;
+
+const TextWrapper = styled.span`
+  margin-bottom: 6px;
 `;
 
 const HEIGHT_CARD = 80;
@@ -30,7 +78,13 @@ const Content = () => {
   return (
     <MainPageContainer>
       <GridContainer>
-        <GridBlock>角色</GridBlock>
+        <GridBlock>
+          <div>
+            <div>角色</div>
+            <div>Male</div>
+            <div>Female</div>
+          </div>
+        </GridBlock>
         {persons.map((person) => (
           <GridBlock
             key={person.name.en}
@@ -38,12 +92,24 @@ const Content = () => {
             backgroundColor={person.color}
             height={HEIGHT_CARD}
           >
-            {person.name.zhtw}
+            <IconWrapper>
+              <Icon icon={person.isMale ? iconMalePath : iconFemalePath} />
+            </IconWrapper>
+            <TextWrapper>
+              <Text>{person.name.zhtw}</Text>
+              <Text>{person.name.en}</Text>
+            </TextWrapper>
           </GridBlock>
         ))}
       </GridContainer>
       <GridContainer>
-        <GridBlock>地點</GridBlock>
+        <GridBlock>
+          <div>
+            <div>地點</div>
+            <div>Indoor</div>
+            <div>Outdoor</div>
+          </div>
+        </GridBlock>
         {locations.map((location) => (
           <GridBlock
             key={location.name.en}
@@ -51,12 +117,28 @@ const Content = () => {
             backgroundColor={location.color}
             height={HEIGHT_CARD}
           >
-            {location.name.zhtw}
+            <IconWrapper>
+              {location.isIndoor ? (
+                <InDoorIcon htmlColor="#000" />
+              ) : (
+                <OutDoorIcon htmlColor="#000" />
+              )}
+            </IconWrapper>
+            <TextWrapper>
+              <Text>{location.name.zhtw}</Text>
+              <Text>{location.name.en}</Text>
+            </TextWrapper>
           </GridBlock>
         ))}
       </GridContainer>
       <GridContainer>
-        <GridBlock>武器</GridBlock>
+        <GridBlock>
+          <div>
+            <div>武器</div>
+            <div>UpClose</div>
+            <div>Ranged</div>
+          </div>
+        </GridBlock>
         {weapons.map((weapon) => (
           <GridBlock
             key={weapon.name.en}
@@ -64,7 +146,17 @@ const Content = () => {
             backgroundColor={weapon.color}
             height={HEIGHT_CARD}
           >
-            {weapon.name.zhtw}
+            <IconWrapper>
+              {weapon.isUpclose ? (
+                <UpCloseIcon htmlColor="#000" />
+              ) : (
+                <RangedIcon fontSize="small" htmlColor="#000" />
+              )}
+            </IconWrapper>
+            <TextWrapper>
+              <Text>{weapon.name.zhtw}</Text>
+              <Text>{weapon.name.en}</Text>
+            </TextWrapper>
           </GridBlock>
         ))}
       </GridContainer>
