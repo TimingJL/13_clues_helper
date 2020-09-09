4a5b52974273a00e51ee79988d45988464a9ed4d
taiming <taiming@bridgewell.com>
2020-09-09 14:18:22 +0800

    [feature] add nav bar

--- src/containers/MainPage/index.js
+++ src/containers/MainPage/index.js
@@ -1,41 +1,28 @@
 import React from "react";
 import styled from "styled-components";
-import data from "./data";
+import Header from "./Header";
+import Content from "./Content";
 
-const MainPageContainer = styled.div``;
-
-const GridContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(11, 1fr);
+const MainPageContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
 `;
 
-const GridBlock = styled.div`
-  border: 1px solid #eee;
-  height: 100px;
+const ContentWrapper = styled.div`
+  flex-grow: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const MainPage = () => {
-  const { persons, locations, weapons } = data;
   return (
     <MainPageContainer>
-      <GridContainer>
-        <GridBlock>Header</GridBlock>
-        {persons.map((person) => (
-          <GridBlock key={person.name.en}>{person.name.zhtw}</GridBlock>
-        ))}
-      </GridContainer>
-      <GridContainer>
-        <GridBlock>Header</GridBlock>
-        {locations.map((location) => (
-          <GridBlock key={location.name.en}>{location.name.zhtw}</GridBlock>
-        ))}
-      </GridContainer>
-      <GridContainer>
-        <GridBlock>Header</GridBlock>
-        {weapons.map((weapon) => (
-          <GridBlock key={weapon.name.en}>{weapon.name.zhtw}</GridBlock>
-        ))}
-      </GridContainer>
+      <Header />
+      <ContentWrapper>
+        <Content />
+      </ContentWrapper>
     </MainPageContainer>
   );
 };
