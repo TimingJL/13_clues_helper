32c1af8a96e8fdd7192892d3f4f64bce0b6ebb99
taiming <taiming@bridgewell.com>
2020-09-10 06:21:40 +0800

    Updates

---
+++ precache-manifest.47ead57c118c9dbb62d614d3eee38f2e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7ab9b0733572cf7f310247b5866c79d",
+    "url": "/13_clues_helper/index.html"
+  },
+  {
+    "revision": "7ba6588fdba5ac5c6de5",
+    "url": "/13_clues_helper/static/css/2.e6cf9162.chunk.css"
+  },
+  {
+    "revision": "095760eb37362d317c7a",
+    "url": "/13_clues_helper/static/css/main.df18e7ce.chunk.css"
+  },
+  {
+    "revision": "7ba6588fdba5ac5c6de5",
+    "url": "/13_clues_helper/static/js/2.49fccdfc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/13_clues_helper/static/js/2.49fccdfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "095760eb37362d317c7a",
+    "url": "/13_clues_helper/static/js/main.af917b57.chunk.js"
+  },
+  {
+    "revision": "d45936075881cd229873",
+    "url": "/13_clues_helper/static/js/runtime-main.28075f96.js"
+  },
+  {
+    "revision": "7edc587601e76cdcca1ce99e96c86ce0",
+    "url": "/13_clues_helper/static/media/13_clues.7edc5876.png"
+  },
+  {
+    "revision": "4f480002f25f701211b9e6e8d0a1e149",
+    "url": "/13_clues_helper/static/media/backgroundImage.4f480002.jpg"
+  },
+  {
+    "revision": "a69582d6409f1bc03be176693f6ddbd5",
+    "url": "/13_clues_helper/static/media/game_accessories.a69582d6.jpg"
+  },
+  {
+    "revision": "ec5073605db50822b7f011c2c6b44d51",
+    "url": "/13_clues_helper/static/media/game_preparation.ec507360.jpg"
+  },
+  {
+    "revision": "ad402ebd4a59f1305debb44461f9abd6",
+    "url": "/13_clues_helper/static/media/game_progress.ad402ebd.jpg"
+  }
+]);
